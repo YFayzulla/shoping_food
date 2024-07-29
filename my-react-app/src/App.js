3192eb5e939a0e49291bb4a56e97b659df699242
YFayzulla <yoldashevFayzulla0@gmail.com>
2024-07-29 21:27:29 +0500

    Button without scc

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,9 +1,16 @@
 import './App.css';
+import Button from './Components/Button/Button';
 
 function App() {
-
-  return ( <h1>im here</h1> ) ; 
-
+    return(
+    <>
+   
+        <h1> im here </h1> 
+        <Button title={'Test'} disable={false} />  
+    
+    </>
+    )
+    
 }
 
 export default App;
